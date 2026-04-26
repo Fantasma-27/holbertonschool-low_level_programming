f918741db669b1b697d9a507a690906c66a8f864
Fantasma-27 <camimarlyn@gmail.com>
2026-04-26 18:09:55 +0000

    betty correction on 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,13 +2,13 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- **
-Return: the int converted from the string
+ *
+*Return: the converted integer
 */
 int _atoi(char *s)
 {
-
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
