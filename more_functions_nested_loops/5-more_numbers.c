786352bf07dcea38a86222753d1e1b5774bd30f1
Fantasma-27 <131152033+Fantasma-27@users.noreply.github.com>
2026-05-14 10:35:37 +0000

    task to print 0-14 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * more_numbers - prints 1-14 10 times
+ *
+ * Return: void 
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			_putchar((y / 10) + 48);
+			_putchar((y % 10) + 48);
+		}
+
+		_putchar('\n');
+	}
+}
