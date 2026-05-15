a29073807a006e695e3e46cf642bdd0a16fe3771
Fantasma-27 <131152033+Fantasma-27@users.noreply.github.com>
2026-05-15 11:42:49 +0000

    print every other character betty code

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,16 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*str != '\0') 
+	while (*str != '\0')
 	{
 		if (i % 2 == 0)
 		{
-		_putchar(*str);
+			_putchar(*str);
 		}
+
 		i++;
 		str++;
-		}
+	}
 
 	_putchar('\n');
 }
