f5fd6cd0d31acb266c284e0858ca7add15fc3976
Fantasma-27 <camimarlyn@gmail.com>
2026-06-25 22:31:30 +0400

    a header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog information structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: stores information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
