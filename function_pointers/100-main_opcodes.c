62ed43434e28da6f5d52bf107c617dd321edd7d4
Fantasma-27 <camimarlyn@gmail.com>
2026-06-27 12:50:30 +0400

    advanced task

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcodes of main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1/2 on error
+ */
+int main(int argc, char *argv[])
+{
+    int n, i;
+    unsigned char *ptr;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    n = atoi(argv[1]);
+    if (n < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+    ptr = (unsigned char *)main;
+    for (i = 0; i < n; i++)
+    {
+        if (i > 0)
+            printf(" ");
+        printf("%02x", ptr[i]);
+    }
+    printf("\n");
+    return (0);
+}
