4f9cb76dcad2307f50d0700d75a651d9b9e28314
Fantasma-27 <camimarlyn@gmail.com>
2026-06-28 10:48:30 +0400

    print both string and int with separator

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,90 @@
+#include "variadic_functions.h"
+#include<stdio.h>
+
+/**
+ * print_char - prints a char
+ * @args: va_list
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ * @args: va_list
+ */
+void print_int(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: va_list
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: va_list
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+
+	else
+		printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: format string
+ */
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	int i, j, first;
+	op_t ops[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	va_start(args, format);
+	i = 0;
+	first = 1;
+
+	while (format[i])
+	{
+		j = 0;
+
+		while (ops[j].c != '\0')
+		{
+			if (format[i] == ops[j].c)
+			{
+				if (first == 0)
+					printf(", ");
+
+				ops[j].f(args);
+				first = 0;
+				break;
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,11 +2,23 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct op - operator struct for print_all
+ * @c: character
+ * @f: function pointer
+ */
+typedef struct op
+{
+    char c;
+    void (*f)(va_list);
+} op_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
+void print_all(const char * const format, ...);
 
 
 #endif
