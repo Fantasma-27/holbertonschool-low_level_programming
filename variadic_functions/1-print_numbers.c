ecdcf2a372363c46b5a0712d5d2cd99db9a7aa7f
Fantasma-27 <camimarlyn@gmail.com>
2026-06-28 10:35:06 +0400

    print numbers with a comma between

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers separated by a separator
+ * @separator: string to print between numbers
+ * @n: number of integers to print
+ * @...: variable number of integers
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+
+		printf("%d", va_arg(args, int));
+	}
+
+	va_end(args);
+	printf("\n");
+}
