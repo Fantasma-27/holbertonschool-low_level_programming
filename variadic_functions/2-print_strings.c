d39ea7c8c10af55c14bbe36f4d629fea35e7e927
Fantasma-27 <camimarlyn@gmail.com>
2026-06-28 10:39:13 +0400

    sting plus separator

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings separated by a separator
+ * @separator: string to print between strings
+ * @n: number of strings to print
+ * @...: variable number of strings
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+
+		else
+			printf("%s", str);
+	}
+
+	va_end(args);
+	printf("\n");
+}
