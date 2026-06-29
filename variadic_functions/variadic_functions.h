db106a4c3a11ab7a417bacd83879bf0d52cb1164
Fantasma-27 <camimarlyn@gmail.com>
2026-06-29 11:59:28 +0400

    betty error on variadic.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,14 +11,14 @@
  */
 typedef struct op
 {
-    char c;
-    void (*f)(va_list);
+	char c;
+	void (*f)(va_list);
 } op_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 
 
 #endif
